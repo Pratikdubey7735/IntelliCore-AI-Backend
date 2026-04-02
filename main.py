b346fb0548ef8ca4402e94087b1c3f2763d47cdd
Pratik Dubey <Pratikdubey7735@gmail.com>
2026-04-02 11:32:42 +0530

    Change in cors

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="Intellicore AI")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://intelli-core-ai-eight.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
